5db61a4f5e692589848156746ee9a4050743c569
dutdavid <dutdavid7@gmail.com>
2023-04-20 20:21:10 +0000

    com

--- custom_video_player/scripts.js
+++ custom_video_player/scripts.js
@@ -43,3 +43,4 @@ let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
