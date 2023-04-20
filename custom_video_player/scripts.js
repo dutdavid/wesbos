69cde8ee12914199ef87692fbb5a40a54014c68b
dutdavid <dutdavid7@gmail.com>
2023-04-20 20:19:55 +0000

    com

--- custom_video_player/scripts.js
+++ custom_video_player/scripts.js
@@ -36,3 +36,4 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
